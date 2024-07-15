fa9fb4bbe87bce1fe87e29f4996938cd1f57980f
Marharyta Skokava <marharyta.skokava@sap.com>
2024-07-15 19:03:37 +0200

    init

---
+++ src/main/java/com/example/helloforazure/Controller.java
@@ -0,0 +1,13 @@
+package com.example.helloforazure;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
